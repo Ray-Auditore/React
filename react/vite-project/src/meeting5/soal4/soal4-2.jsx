6717caa166df4441c01aedd03510643037f53c53
Xyasskangboyah <151702591+Ray-Auditore@users.noreply.github.com>
2024-11-26 11:06:47 +0700

    tugas 5

---
+++ react/vite-project/src/meeting5/soal4/soal4-2.jsx
@@ -0,0 +1,11 @@
+import Greeting  from "./soal4";
+export default function time() {
+    return (
+        <div>
+            <greeting time={11} />
+            <greeting time={14} />
+            <greeting time={19} />
+            <greeting time={2} />
+        </div>
+    )
+}
