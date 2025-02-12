5820aadb6a7d17f95b4f2f08589b8326d7414d14
Rayyaccc_ <151702591+Ray-Auditore@users.noreply.github.com>
2025-02-12 13:48:46 +0700

    save

--- react/vite-project/src/main.jsx
+++ react/vite-project/src/main.jsx
@@ -3,12 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 
 import ShoppingCard from "./meeting9/praktek/praktek1.jsx";
+import SimpleNameList from "./meeting9/updateState.jsx";
+import UpdateStateWithMap from "./meeting9/updateState2.jsx";
 
 
 
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <ShoppingCard/>
+    <UpdateStateWithMap/>
   </StrictMode>
 );
